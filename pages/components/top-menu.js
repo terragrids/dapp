0d56dfd4c4d5fe83aaf6d5a346041e0e40221914
Lorenzo Polidori <lorenzopolidori@gmail.com>
2022-03-18 22:03:41 +0000

    Dynamically load reach libraries

--- pages/components/top-menu.js
+++ pages/components/top-menu.js
@@ -1,26 +1,29 @@
 import { strings } from '../../strings/en'
 import styles from './top-menu.module.scss'
-import { loadStdlib } from '@reach-sh/stdlib'
-import React, { useRef } from 'react'
-
-let MyAlgoConnect
-import('@reach-sh/stdlib/ALGO_MyAlgoConnect').then((module) => {
-    MyAlgoConnect = module.default
-})
+import React, { useRef, useEffect } from 'react'
 
 export default function TopMenu() {
-    const reach = useRef(loadStdlib({ ...process.env, 'REACH_CONNECTOR_MODE': process.env.NEXT_PUBLIC_REACH_CONNECTOR_MODE }))
+    const reach = useRef()
 
     async function connectWallet() {
-        reach.current.setWalletFallback(reach.current.walletFallback({
-            providerEnv: 'TestNet', MyAlgoConnect
-        }))
-
         const acc = await reach.current.getDefaultAccount()
-
-        const balAtomic = await reach.current.balanceOf(acc)
-        reach.current.formatCurrency(balAtomic, 4)
+        const balance = await reach.current.balanceOf(acc)
+        reach.current.formatCurrency(balance, 4)
     }
+
+    useEffect(() => {
+        async function loadLibs() {
+            let [reachStdlib, myAlgoConnect] = await Promise.all([import('@reach-sh/stdlib'), import('@reach-sh/stdlib/ALGO_MyAlgoConnect')])
+
+            const MyAlgoConnect = myAlgoConnect.default
+
+            reach.current = reachStdlib.loadStdlib({ ...process.env, 'REACH_CONNECTOR_MODE': process.env.NEXT_PUBLIC_REACH_CONNECTOR_MODE })
+            reach.current.setWalletFallback(reach.current.walletFallback({
+                providerEnv: 'TestNet', MyAlgoConnect
+            }))
+        }
+        loadLibs()
+    }, [])
 
     return (
         <nav>
