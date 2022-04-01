964e11863d9643e9a5786746051df50a2e20055e
Lorenzo Polidori <lorenzopolidori@gmail.com>
2022-04-01 22:36:31 +0100

    Upgrade reach stdlib

--- hooks/use-reach.js
+++ hooks/use-reach.js
@@ -4,20 +4,26 @@ import * as backend from '../build/index.main.mjs'
 export function useReach() {
     const stdlib = useRef()
     const myAlgoConnect = useRef()
+    const walletConnect = useRef()
 
     const [loading, setLoading] = useState(true)
 
     useEffect(() => {
         async function loadLibs() {
-            let [reachStdlib, myAlgoConnectLib] = await Promise.all([
-                import('@reach-sh/stdlib'),
-                import('@reach-sh/stdlib/ALGO_MyAlgoConnect')])
+            const reachStdlib = await import('@reach-sh/stdlib')
             stdlib.current = reachStdlib.loadStdlib({ ...process.env, 'REACH_CONNECTOR_MODE': process.env.NEXT_PUBLIC_REACH_CONNECTOR_MODE })
-            myAlgoConnect.current = myAlgoConnectLib.default
+            myAlgoConnect.current = reachStdlib.ALGO_MyAlgoConnect
+            walletConnect.current = reachStdlib.ALGO_WalletConnect
             setLoading(false)
         }
         loadLibs()
     }, [])
 
-    return { backend, stdlib: stdlib.current, myAlgoConnect: myAlgoConnect.current, loading }
+    return {
+        backend,
+        stdlib: stdlib.current,
+        myAlgoConnect: myAlgoConnect.current,
+        walletConnect: walletConnect.current,
+        loading
+    }
 }
