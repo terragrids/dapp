6fb5f163085a3d83bc7e4691c9f7514d9160cbd0
Lorenzo Polidori <lorenzopolidori@gmail.com>
2022-05-04 10:08:16 +0100

    Use plain next api handler for terracells endpoint

--- pages/api/terracells.js
+++ pages/api/terracells.js
@@ -1,22 +1,23 @@
-import nc from 'next-connect'
-import cors from 'cors'
 import { algorandIndexerBaseUrl } from './config'
 
-const handler = nc()
-    .use(cors())
-    .get(async (req, res) => {
-        const response = await fetch(`${algorandIndexerBaseUrl}/assets?unit=TRCL`)
-        const json = await response.json()
-        res.send({
-            assets: json.assets
-                .filter(asset => !asset.deleted && asset.params.total === 1 && asset.params.decimals === 0)
-                .map(asset => ({
-                    id: asset.index,
-                    name: asset.params.name,
-                    symbol: asset.params['unit-name'],
-                    url: asset.params.url
-                }))
-        })
-    })
-
-export default handler
+export default async function handler(req, res) {
+    switch (req.method) {
+        case 'GET':
+            const response = await fetch(`${algorandIndexerBaseUrl}/assets?unit=TRCL`)
+            const json = await response.json()
+            res.status(200).json({
+                assets: json.assets
+                    .filter(asset => !asset.deleted && asset.params.total === 1 && asset.params.decimals === 0)
+                    .map(asset => ({
+                        id: asset.index,
+                        name: asset.params.name,
+                        symbol: asset.params['unit-name'],
+                        url: asset.params.url
+                    }))
+            })
+            break
+        default:
+            res.setHeader('Allow', ['GET'])
+            res.status(405).end()
+    }
+}
