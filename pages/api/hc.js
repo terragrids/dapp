70468821e9f8d462f941a87db3910e9520228441
Lorenzo Polidori <lorenzopolidori@gmail.com>
2022-07-15 23:04:37 +0100

    Remove pinata

--- pages/api/hc.js
+++ pages/api/hc.js
@@ -1,28 +1,22 @@
-import IpfsRepository from '../../repository/ipfs.repository.js'
 import { handleHttpRequest, setMethodNotAllowedResponse, terragridsApiBaseUrl } from '../../utils/api-config'
 
 export default async function handler(req, res) {
     switch (req.method) {
         case 'GET':
             await handleHttpRequest(res, async () => {
-                const [apiResponse, ipfsResponse] = await Promise.all([
-                    fetch(`${terragridsApiBaseUrl}/hc`),
-                    new IpfsRepository().testConnection()
-                ].map(p => p.catch(e => e)))
-
                 let apiStatus
-                if (apiResponse instanceof Error) {
-                    apiStatus = { error: apiResponse.message }
-                } else {
+                try {
+                    const apiResponse = await fetch(`${terragridsApiBaseUrl}/hc`)
                     apiStatus = apiResponse.status === 200 ? await apiResponse.json() :
                         { error: { httpStatus: apiResponse.status, httpBody: await apiResponse.text() } }
+                } catch (e) {
+                    apiStatus = { error: e.message }
                 }
 
                 res
                     .status(200)
                     .send({
-                        api: apiStatus,
-                        ipfs: ipfsResponse
+                        api: apiStatus
                     })
             })
             break
