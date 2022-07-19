7b980796f23225cdc0b406639e9d58b4007eebb0
Lorenzo Polidori <lorenzopolidori@gmail.com>
2022-07-19 14:55:00 +0100

    Add draft Error component

---
+++ components/error.tsx
@@ -0,0 +1,13 @@
+import styles from './error.module.scss'
+
+export const Error = ({ message }: Props) => {
+    return (
+        <div role={'alert'} className={styles.container} >
+            <div>{message} </div>
+        </div>
+    )
+}
+
+type Props = {
+    message: string
+};
