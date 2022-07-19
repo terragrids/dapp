a8f0f186940ebe2ab72753f52c0e2e2cea3cc9f1
Lorenzo Polidori <lorenzopolidori@gmail.com>
2022-07-19 15:26:36 +0100

    Add ImageUploader stub

---
+++ components/image-uploader.tsx
@@ -0,0 +1,9 @@
+import styles from './image-uploader.module.scss'
+
+export const ImageUploader = () => {
+    return (
+        <div className={styles.container} >
+            <div></div>
+        </div>
+    )
+}
