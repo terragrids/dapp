b8d66e5e8381cba27655f46ffcc7028590621191
Lorenzo Polidori <lorenzopolidori@gmail.com>
2022-08-02 08:20:09 +0200

    Rename api folders

--- pages/api/terracells/[assetId].js
+++ pages/api/terracells/[assetId].js
@@ -1,9 +1,10 @@
+/* eslint-disable unicorn/filename-case */
 import { callTerragridsApi, setMethodNotAllowedResponse } from '../../../utils/api-config'
 
 export default async function handler(req, res) {
     switch (req.method) {
         case 'GET':
-            await callTerragridsApi(res, 'GET', `terracells/${req.query.assetid}`)
+            await callTerragridsApi(res, 'GET', `terracells/${req.query.assetId}`)
             break
         default:
             setMethodNotAllowedResponse(res, ['GET'])
--- pages/api/terracells/[assetId]/contracts/[applicationId].js
+++ pages/api/terracells/[assetId]/contracts/[applicationId].js
@@ -1,12 +1,13 @@
+/* eslint-disable unicorn/filename-case */
 import { callTerragridsApi, setMethodNotAllowedResponse } from '../../../../../utils/api-config'
 
 export default async function handler(req, res) {
     switch (req.method) {
         case 'POST':
-            await callTerragridsApi(res, 'POST', `terracells/${req.query.assetid}/contracts/${req.query.applicationid}`, req.body)
+            await callTerragridsApi(res, 'POST', `terracells/${req.query.assetId}/contracts/${req.query.applicationId}`, req.body)
             break
         case 'DELETE':
-            await callTerragridsApi(res, 'DELETE', `terracells/${req.query.assetid}/contracts/${req.query.applicationid}`)
+            await callTerragridsApi(res, 'DELETE', `terracells/${req.query.assetId}/contracts/${req.query.applicationId}`)
             break
         default:
             setMethodNotAllowedResponse(res, ['POST', 'DELETE'])
