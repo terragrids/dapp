e3f3f68251612fbb4aa0299cedceba586ec93cd0
masafumimori <m.masafumimori@outlook.com>
2022-08-04 12:46:16 +0900

    display tile info in modal

--- components/map/tiles/tile-info-dialog.tsx
+++ components/map/tiles/tile-info-dialog.tsx
@@ -4,44 +4,50 @@ import styles from './tile-info-dialog.module.scss'
 import React from 'react'
 import Button from 'components/button'
 import { ImagePicker } from 'components/image-picker'
-import { TileInfo } from 'components/map'
 import { strings } from 'strings/en'
+import { removeSuffix, shortenAddress, TRDL_SUFFIX } from './tile-helpers'
 
 type TileInfoDialogProps = {
     visible: boolean
     onClose: () => void
-    tileInfo: TileInfo | undefined
+    tileInfo: TileMapType | undefined
 }
 
-const TileInfoDialog = ({
-    visible,
-    onClose,
-    tileInfo
-}: TileInfoDialogProps) => {
+const TileInfoDialog = ({ visible, onClose, tileInfo }: TileInfoDialogProps) => {
     if (!tileInfo) return null
 
+    const { name, coord, offchainUrl, holders } = tileInfo
+
     return (
-        <ModalDialog
-            visible={visible}
-            title={strings.terralandInformation}
-            onClose={onClose}>
+        <ModalDialog visible={visible} title={strings.terralandInformation} onClose={onClose}>
             <div className={styles.container}>
                 <div className={styles.section}>
                     {/* Can remove `ImagePicker` once this dialog is fully implemented
                        for now just to show if the right tile is selected */}
                     <ImagePicker
-                        url={tileInfo.imageUrl}
-                        onFilesPicked={(array) => {
+                        url={offchainUrl}
+                        onFilesPicked={array => {
                             return array
                         }}
                     />
                 </div>
                 <div className={styles.section}>
+                    <p>Name: {removeSuffix(name, TRDL_SUFFIX)}</p>
                     <p>Tile index : {tileInfo.index}</p>
                     <p>
-                        Tile xCoord : {tileInfo.xCoord} / yCoord :{' '}
-                        {tileInfo.yCoord}
+                        Tile xCoord : {coord.x} / yCoord : {coord.y}
                     </p>
+                    <div className={styles.section}>
+                        <h4>Holders</h4>
+                        <ul>
+                            {holders.map(holder => (
+                                <li style={{ listStyle: 'none' }} key={holder.address + holder.amount}>
+                                    <p>- address : {shortenAddress(holder.address)}</p>
+                                    <p>- amount: {holder.amount}</p>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
                 </div>
                 <Button
                     className={styles.button}
