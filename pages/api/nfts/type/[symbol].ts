3e08bcbf207970fd7c07e2ca8de9f109e1dbf375
Lorenzo Polidori <lorenzopolidori@gmail.com>
2022-08-06 23:05:21 +0200

    Cleanup api

--- pages/api/nfts/type/[symbol].ts
+++ pages/api/nfts/type/[symbol].ts
@@ -1,26 +1,13 @@
 /* eslint-disable unicorn/filename-case */
 import { NextApiRequest, NextApiResponse } from 'next/types'
 import { callTerragridsApi, setMethodNotAllowedResponse } from 'utils/api-config'
-import { NFT_MOCKS } from '../../../../mocks/nfts/type/symbol'
-
-type Data = {
-    assets?:
-    | typeof NFT_MOCKS['trld']['assets']
-    | typeof NFT_MOCKS['trcl']['assets']
-    'next-token'?: string
-}
 
 export default async function handler(
     req: NextApiRequest,
-    res: NextApiResponse<Data>
+    res: NextApiResponse
 ) {
     switch (req.method) {
         case 'GET':
-            // if (matchKey(req.query.symbol)) {
-            //     const response = NFT_MOCKS[req.query.symbol]
-            //     return res.status(200).json(response)
-            // }
-            // return res.status(404).end()
             await callTerragridsApi(res, 'GET', `nfts/type/${req.query.symbol}`)
             break
         default:
