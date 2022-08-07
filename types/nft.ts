e5a7a5afd6fd11c7108cb117aa54917b2fb42b93
masafumimori <m.masafumimori@outlook.com>
2022-08-08 07:25:25 +0900

    add types

--- types/nft.ts
+++ types/nft.ts
@@ -1,35 +1,53 @@
 export class Nft {
-    static readonly TRCL = new Nft('TRCL', 'Terracell')
-    static readonly TRLD = new Nft('TRLD', 'Terraland')
-    static readonly TRAS = new Nft('TRAS', 'Terrasset')
+  static readonly TRCL = new Nft('TRCL', 'Terracell')
+  static readonly TRLD = new Nft('TRLD', 'Terraland')
+  static readonly TRAS = new Nft('TRAS', 'Terrasset')
 
-    currencySymbol: string
+  currencySymbol: string
 
-    // private to disallow creating other instances of this type
-    private constructor(public readonly symbol: string, public readonly name: string) {
-        this.currencySymbol = `$${this.symbol}`
-    }
+  // private to disallow creating other instances of this type
+  private constructor(public readonly symbol: string, public readonly name: string) {
+    this.currencySymbol = `$${this.symbol}`
+  }
 
-    toString() {
-        return `${this.name} (${this.currencySymbol})`
-    }
+  toString() {
+    return `${this.name} (${this.currencySymbol})`
+  }
 
-    static list() {
-        return [Nft.TRCL, Nft.TRLD, Nft.TRAS]
-    }
+  static list() {
+    return [Nft.TRCL, Nft.TRLD, Nft.TRAS]
+  }
 }
+
+export type Symbols = 'TRCL' | 'TRLD' | 'TRAS'
 
 export type Holder = {
-    address: string
-    amount: number
+  address: string
+  amount: number
 }
 export type Terraland = {
-    id: string
-    name: string
-    symbol: string
-    url: string
-    offchainUrl: string
-    positionX: number
-    positionY: number
-    holders: Array<Holder>
+  id: string
+  name: string
+  symbol: string
+  url: string
+  offchainUrl: string
+  positionX: number
+  positionY: number
+  holders: Array<Holder>
+}
+
+export type SolarPowerPlant = {
+  capacity: number
+  output: number
+  totalTrcl: number
+  activeTrcl: number
+}
+
+export type Terracell = {
+  id: number
+  name: string
+  symbol: Symbols
+  url: string
+  offchainUrl: string
+  power: number
 }
