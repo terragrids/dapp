c7d8fc7fe53e9b9fe961a28619c54c4a744baca7
Lorenzo Polidori <lorenzopolidori@gmail.com>
2022-08-12 15:47:47 +0200

    Fix modal-dialog formatting

--- components/modal-dialog.js
+++ components/modal-dialog.js
@@ -2,36 +2,36 @@ import styles from './modal-dialog.module.scss'
 import PropTypes from 'prop-types'
 
 export default function ModalDialog({ visible, title, children, onClose, subtitle, className }) {
-  return visible ? (
-    <div className={styles.container}>
-      <div className={`${styles.dialog} ${className}`}>
-        {(title || onClose) && (
-          <header className={styles.header}>
-            <h1 className={styles.title}>
-              {title}
-              {subtitle && <small>{subtitle}</small>}
-            </h1>
+    return visible ? (
+        <div className={styles.container}>
+            <div className={`${styles.dialog} ${className}`}>
+                {(title || onClose) && (
+                    <header className={styles.header}>
+                        <h1 className={styles.title}>
+                            {title}
+                            {subtitle && <small>{subtitle}</small>}
+                        </h1>
 
-            {onClose && (
-              <div>
-                <i className={`${styles.close} icon-cross`} onClick={onClose} />
-              </div>
-            )}
-          </header>
-        )}
-        <section className={styles.content}>{children}</section>
-      </div>
-    </div>
-  ) : (
-    <div />
-  )
+                        {onClose && (
+                            <div>
+                                <i className={`${styles.close} icon-cross`} onClick={onClose} />
+                            </div>
+                        )}
+                    </header>
+                )}
+                <section className={styles.content}>{children}</section>
+            </div>
+        </div>
+    ) : (
+        <div />
+    )
 }
 
 ModalDialog.propTypes = {
-  visible: PropTypes.bool,
-  title: PropTypes.string,
-  children: PropTypes.node,
-  onClose: PropTypes.func,
-  subtitle: PropTypes.string,
-  className: PropTypes.string
+    visible: PropTypes.bool,
+    title: PropTypes.string,
+    children: PropTypes.node,
+    onClose: PropTypes.func,
+    subtitle: PropTypes.string,
+    className: PropTypes.string
 }
