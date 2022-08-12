a54a906044ccc3bb8117d07f5bf56c9467741a18
masafumimori <m.masafumimori@outlook.com>
2022-08-12 19:24:52 +0900

    save with `tabWidth` 4

--- components/solar-power-plant/terracell-detail.tsx
+++ components/solar-power-plant/terracell-detail.tsx
@@ -5,48 +5,54 @@ import { Terracell } from 'types/nft'
 import styles from './terracell-detail.module.scss'
 
 type TerracellDetailProps = {
-  terracell: Terracell | undefined
-  visible: boolean
-  onClose: () => void
+    terracell: Terracell | undefined
+    visible: boolean
+    onClose: () => void
 }
 const TerracellDetail = ({ terracell, onClose }: TerracellDetailProps) => {
-  if (!terracell) return null
+    if (!terracell) return null
 
-  return (
-    <>
-      <span className={styles.back} onClick={onClose}>
-        {strings.back}
-      </span>
-      <div className={styles.container}>
-        <div className={styles.content}>
-          <div className={styles.stack}>
-            <h2>{terracell.name}</h2>
-            <dl>
-              <dt>
-                Owner: <dd>{terracell.id}</dd>
-              </dt>
-              <dt>
-                Price: <dd>{terracell.id} $ALGO</dd>
-              </dt>
-              <dt>
-                Output: <dd>{terracell.power} TRW</dd>
-              </dt>
-              <dt>
-                Type: <dd>{terracell.id}</dd>
-              </dt>
-            </dl>
-            <Button className={styles.default} disabled={false} label={strings.buy} loading={false} onClick={onClose} />
-          </div>
-          <div className={styles.image}>
-            <picture>
-              <source srcSet={terracell.offchainUrl} type={'image/*'} />
-              <img src={terracell.offchainUrl} alt={terracell.name} width={'190px'} height={'auto'} />
-            </picture>
-          </div>
-        </div>
-      </div>
-    </>
-  )
+    return (
+        <>
+            <span className={styles.back} onClick={onClose}>
+                {strings.back}
+            </span>
+            <div className={styles.container}>
+                <div className={styles.content}>
+                    <div className={styles.stack}>
+                        <h2>{terracell.name}</h2>
+                        <dl>
+                            <dt>
+                                Owner: <dd>{terracell.id}</dd>
+                            </dt>
+                            <dt>
+                                Price: <dd>{terracell.id} $ALGO</dd>
+                            </dt>
+                            <dt>
+                                Output: <dd>{terracell.power} TRW</dd>
+                            </dt>
+                            <dt>
+                                Type: <dd>{terracell.id}</dd>
+                            </dt>
+                        </dl>
+                        <Button
+                            className={styles.default}
+                            disabled={false}
+                            label={strings.buy}
+                            loading={false}
+                            onClick={onClose}
+                        />
+                    </div>
+                    <div className={styles.image}>
+                        <picture>
+                            <source srcSet={terracell.offchainUrl} type={'image/*'} />
+                            <img src={terracell.offchainUrl} alt={terracell.name} width={'190px'} height={'auto'} />
+                        </picture>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default TerracellDetail
