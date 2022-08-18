1818468840c4d37d37d18cd1b483be24dc126881
masafumimori <m.masafumimori@outlook.com>
2022-08-18 15:27:42 +0900

    add `usePan` hook

---
+++ hooks/use-pan.ts
@@ -0,0 +1,53 @@
+import { Position2D } from 'components/map/plots/plot'
+import { useState, useRef, useCallback, useEffect } from 'react'
+
+const ORIGIN = Object.freeze({ x: 0, y: 0 })
+
+export default function usePan(): [Position2D, (e: MouseEvent) => void] {
+    const [panState, setPanState] = useState<Position2D>(ORIGIN)
+
+    const lastPointRef = useRef<Position2D>(ORIGIN)
+    const lastOffsetRef = useRef<Position2D>(ORIGIN)
+
+    const pan = useCallback((e: MouseEvent) => {
+        const lastPoint = lastPointRef.current
+        const currentPoint = { x: e.pageX, y: e.pageY }
+        lastPointRef.current = currentPoint
+
+        const mouseDiff = diffPoints(currentPoint, lastPoint)
+
+        setPanState(prev => {
+            const offset = addPoints(prev, mouseDiff)
+            const offsetDiff = diffPoints(offset, lastOffsetRef.current)
+
+            return offsetDiff
+        })
+    }, [])
+
+    useEffect(() => {
+        lastOffsetRef.current = panState
+    }, [panState])
+
+    const endPan = useCallback(() => {
+        document.removeEventListener('mousemove', pan)
+        document.removeEventListener('mouseup', endPan)
+    }, [pan])
+
+    const startPan = useCallback(
+        (e: MouseEvent) => {
+            document.addEventListener('mousemove', pan)
+            document.addEventListener('mouseup', endPan)
+            lastPointRef.current = { x: e.pageX, y: e.pageY }
+        },
+        [pan, endPan]
+    )
+
+    return [panState, startPan]
+}
+
+function diffPoints(p1: Position2D, p2: Position2D) {
+    return { x: p1.x - p2.x, y: p1.y - p2.y }
+}
+function addPoints(p1: Position2D, p2: Position2D) {
+    return { x: p1.x + p2.x, y: p1.y + p2.y }
+}
